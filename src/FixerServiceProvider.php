b763ebb5287247c58c54bb6f1aa13df70238d554
Bubba <rob@stechstudio.com>
2021-05-10 11:52:52 -0400

    Clean up the service provider with some refactoring.

--- src/FixerServiceProvider.php
+++ src/FixerServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace STS\Fixer;
 
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
 use STS\Fixer\Console\FixCommand;
 
@@ -22,41 +23,57 @@ class FixerServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->publish();
+        $this->addCommands();
+    }
+
+    public function register()
+    {
+        $this->config();
+    }
+
+    /**
+     * Add any package commands if we are running in the console.
+     * 
+     * @return void 
+     */
+    protected function addCommands()
+    {
         if ($this->app->runningInConsole()) {
             $this->commands([
                 FixCommand::class,
             ]);
         }
+    }
 
+    /**
+     * Handles configuation for Luman or Laravel
+     * 
+     * @return void 
+     */
+    protected function config()
+    {
+        if (is_a($this->app, 'Laravel\Lumen\Application')) {
+            $this->app->configure('fixer');
+        }
+        $this->mergeConfigFrom($this->configPath, 'fixer');
+    }
+
+    /**
+     * Handles publishing the configuation class for Luman or Laravel
+     * 
+     * @return void 
+     * @throws BindingResolutionException 
+     */
+    protected function publish()
+    {
         // helps deal with Lumen vs Laravel differences
         if (function_exists('config_path')) {
             $publishPath = config_path('fixer.php');
         } else {
             $publishPath = base_path('config/fixer.php');
         }
+
         $this->publishes([$this->configPath => $publishPath], 'config');
-    }
-
-    public function register()
-    {
-        if (is_a($this->app, 'Laravel\Lumen\Application')) {
-            $this->app->configure('fixer');
-        }
-        $this->mergeConfigFrom($this->configPath, 'fixer');
-
-        $this->app->singleton('command.fixer.fix', function ($app) {
-            return new FixCommand();
-        });
-        $this->commands('command.fixer.fix');
-    }
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return ['command.fixer.fix'];
     }
 }
