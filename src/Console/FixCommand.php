d2972268244eab231bd9cdfeaf4a5597a92874b7
Bubba <rob@stechstudio.com>
2021-05-11 15:02:41 +0000

    Ignore that

--- src/Console/FixCommand.php
+++ src/Console/FixCommand.php
@@ -48,15 +48,6 @@ class FixCommand extends Command
         $this->setDescription($fixerCommand->getDescription());
         $this->fixerApplication = new \PhpCsFixer\Console\Application();
         $this->longVersion = $this->fixerApplication->getLongVersion();
-
-
-
-        // 'v' => OutputInterface::VERBOSITY_VERBOSE,
-        // 'vv' => OutputInterface::VERBOSITY_VERY_VERBOSE,
-        // 'vvv' => OutputInterface::VERBOSITY_DEBUG,
-        // 'quiet' => OutputInterface::VERBOSITY_QUIET,
-        // 'normal' => OutputInterface::VERBOSITY_NORMAL,
-        //$this->setVerbosity();
     }
 
     public function handle()
