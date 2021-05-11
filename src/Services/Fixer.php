cd5c5a8887eafdbf726d76c298ea766743cc18bd
Bubba <rob@stechstudio.com>
2021-05-11 14:58:11 +0000

    Provide Messages

--- src/Services/Fixer.php
+++ src/Services/Fixer.php
@@ -9,11 +9,11 @@ use PhpCsFixer\ToolInfo;
 use PhpCsFixer\ToolInfoInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use PhpCsFixer\Console\Output\ErrorOutput;
 use PhpCsFixer\Console\Output\NullOutput;
 use PhpCsFixer\Console\Output\ProcessOutput;
 use PhpCsFixer\Finder;
 use Symfony\Component\Console\Terminal;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Fixer
 {
@@ -25,7 +25,7 @@ class Fixer
 
     /**
      * 
-     * @var mixed
+     * @var ConfigurationResolver
      */
     protected $configResolver;
 
@@ -60,13 +60,19 @@ class Fixer
         $this->output = $output;
         $this->cliInput = $cliInput;
         $this->setResolver($cliInput, $config, $cwd, $toolInfo);
+        $this->setEventDispatcher();
         $this->setFinder();
         $this->setProgressOutput();
     }
 
+    public function setEventDispatcher(?EventDispatcher $eventDispatcher = null)
+    {
+        $this->eventDispatcher = $eventDispatcher ?? new EventDispatcher();
+    }
+
     public function setResolver(array $options, ?ConfigInterface $config = null, ?string $cwd = null, ?ToolInfoInterface $toolInfo = null)
     {
-        $config = $config ?: new Config();
+        $config = $config ?: new Config(config('fixer.config_name', 'Laravel'));
         $cwd = $cwd ?: getcwd();
         $toolInfo = $toolInfo ?: new ToolInfo();
         $this->configResolver = new ConfigurationResolver($config, $options, $cwd, $toolInfo);
@@ -76,10 +82,25 @@ class Fixer
     {
         $this->finder = Finder::create()
             ->in(config('fixer.find_directories'))
-            ->name(config('file_name_pattern_whitelist'))
-            ->notName(config('file_name_pattern_blacklist'))
-            ->ignoreDotFiles(config('ignore_dot_files'))
-            ->ignoreVCS(config('ignore_vcs'));
+            ->name(config('fixer.file_name_pattern_whitelist'))
+            ->notName(config('fixer.file_name_pattern_blacklist'))
+            ->ignoreDotFiles(config('fixer.ignore_dot_files'))
+            ->ignoreVCS(config('fixer.ignore_vcs'));
+    }
+
+    public function getLoadedConfigMessage()
+    {
+        return sprintf(
+            'Loaded config <comment>%s</comment> from %s.',
+            $this->configResolver->getConfig()->getName(),
+            $this->configResolver->getConfigFile() ?? 'Laravel Fixer Configuration.'
+        );
+    }
+
+    public function getPhpRuntimeMessage()
+    {
+        $configFile = $this->configResolver->getConfig()->getPhpExecutable();
+        return sprintf('Runtime: <info>PHP %s</info>', PHP_VERSION);
     }
 
     /**
@@ -92,12 +113,11 @@ class Fixer
         $this->progressOutput = new NullOutput();
 
         if ('none' !== $this->configResolver->getProgress()) {
-            $nbFiles = new \ArrayIterator(iterator_to_array($this->finder));
             $this->progressOutput = new ProcessOutput(
                 $this->output,
                 $this->eventDispatcher,
-                'estimating-max' === $this->configResolver->getProgress() ? (new Terminal())->getWidth() : null,
-                count($nbFiles)
+                (new Terminal())->getWidth(),
+                $this->finder->count()
             );
         }
     }
